cbc0de05d0f0da75d8138b0f18c703336900815f
Riwaj-Pokhrel <riwajpokhrel99@gmail.com>
2026-01-12 13:10:35 +0545

    Basics of Python

---
+++ Basic_python/my_modules.py
@@ -0,0 +1,9 @@
+def generate_full_name(first_name,last_name):
+    return first_name+' '+last_name
+def sum_of_numbers(*nums):
+    total=0
+    for i in nums:
+        total+=i
+    return total
+    
+    
